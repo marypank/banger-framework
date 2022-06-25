5ec67164972a475b7c3cde493da19830655b809d
marypank <qitelopdo@gmail.com>
2022-06-25 21:19:16 +0300

    start point of app with composer

---
+++ core/Router.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\core;
+
+class Router
+{
+    public Request $request;
+    protected array $routes = [];
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function get(string $path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+
+        if (!$callback) {
+            echo 'NOT FOUND';
+            exit;
+        }
+
+        echo call_user_func($callback);
+    }
+}
