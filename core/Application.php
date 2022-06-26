7fd1836db98418cd62223990c4c77a5b36fc3b19
marypank <qitelopdo@gmail.com>
2022-06-26 19:54:22 +0300

    implement views and layouts

--- core/Application.php
+++ core/Application.php
@@ -7,14 +7,17 @@ class Application
     public Router $router;
     public Request $request;
 
-    public function __construct()
+    public static string $ROOT_DIR;
+
+    public function __construct($rootPath)
     {
+        self::$ROOT_DIR = $rootPath;
         $this->request = new Request();
         $this->router = new Router($this->request);
     }
 
     public function run()
     {
-        $this->router->resolve();
+        echo $this->router->resolve();
     }
 }
--- core/Router.php
+++ core/Router.php
@@ -19,15 +19,47 @@ class Router
 
     public function resolve()
     {
+        /*
+        it can take
+            1. a function
+            2. a string to render view
+            3. a class and a method in this class
+         */
         $path = $this->request->getPath();
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false;
 
         if (!$callback) {
-            echo 'NOT FOUND';
-            exit;
+            return 'NOT FOUND';
         }
 
-        echo call_user_func($callback);
+        if (is_string($callback)) {
+            $this->renderView($callback);
+        }
+
+        return call_user_func($callback);
+    }
+
+    private function renderView($view)
+    {
+        // todo: user can create many folders with many views, so layouts too (make this)
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+    }
+
+    private function layoutContent()
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    private function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/{$view}.php";
+        return ob_get_clean();
     }
 }
