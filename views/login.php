8f7e10d1c25537b80b4384afaea778eb8f186478
marypank <qitelopdo@gmail.com>
2022-07-06 19:15:42 +0300

    auth pages some style changes

--- views/login.php
+++ views/login.php
@@ -1,16 +1,20 @@
-<h1>Contact page</h1>
-<form action="" method="POST">
-    <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email">
-    </div>
-    <div class="mb-3">
-        <label for="text" class="form-label">Subject</label>
-        <input type="text" name="text" class="form-control" id="text">
-    </div>
-    <div class="mb-3">
-        <label for="textarea" class="form-label">Body</label>
-        <textarea name="textarea" class="form-control" id="textarea"></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<div class="container">
+	<div class="row align-items-center">
+		<div class="col"></div>
+		<div class="col">
+			<h1>Login</h1>
+			<form action="" method="POST">
+				<div class="mb-3">
+					<label for="email" class="form-label">Email</label>
+					<input type="email" class="form-control" id="email" name="email">
+				</div>
+				<div class="mb-3">
+					<label for="password" class="form-label">Password</label>
+					<input type="password" class="form-control" id="password" name="password">
+				</div>
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</form>
+		</div>
+		<div class="col"></div>
+	</div>
+</div>
--- views/register.php
+++ views/register.php
@@ -9,7 +9,7 @@
 					<input type="text" class="form-control" id="username" name="username">
 				</div>
 				<div class="mb-3">
-					<label for="email" class="form-label">First Name</label>
+					<label for="email" class="form-label">Email</label>
 					<input type="email" class="form-control" id="email" name="email">
 				</div>
 				<div class="mb-3">
