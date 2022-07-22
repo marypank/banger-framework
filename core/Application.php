c94d03f0d4d66c1f616d6fc70bd493a5507c55c4
marypank <qitelopdo@gmail.com>
2022-07-22 19:46:32 +0300

    start of migrations and connection to the db

--- core/Application.php
+++ core/Application.php
@@ -8,6 +8,7 @@ class Application
     public Request $request;
     public Response $response;
     public Controller $controller;
+    public Database $db;
 
     public static Application $APP;
     public static string $ROOT_DIR;
@@ -19,6 +20,8 @@ class Application
         $this->request = new Request();
         $this->response = new Response();
         $this->router = new Router($this->request, $this->response);
+
+        $this->db = new Database();
     }
 
     public function run()
---
+++ migrations/m0001_something.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\migrations;
+
+class m0001_something
+{
+	public function up()
+	{
+		echo 'Applying migration';
+	}
+
+	public function down()
+	{
+		echo 'Down migration';
+	}
+}
